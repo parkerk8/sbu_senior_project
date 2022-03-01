bd7bc4a280a0a309a4e1fc73f0a7b38499bb0347
Jacob Knudsen <JakeKnud212@hotmail.com>
2022-02-28 19:04:38 -0600

    Added token creation to server.js

--- Trevor/src/server.js
+++ Trevor/src/server.js
@@ -17,10 +17,33 @@ const { sendAuthUrl } = require('./middleware/auth-sender');
 
 app.use(bodyParser.json()) //this is so we can deal with JSON info in POST requests
 
-app.use(function(req, res, next) {
-  console.log(req.method + " " + req.path + " - " + req.ip);
-  next();
-});
+app.use(function (req, res, next) {
+    //Creates a new token or detects if a token already exists
+    const client_id = "232811749250-phji8o1bmnd86b3vff1uetdkp12138vi.apps.googleusercontent.com";
+    const client_secret = "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";
+    const redirect_uri = "http://localhost:3000";
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uri);
+    //Creates a token from the recieved auth code
+    if (!("./token.json")) {
+        const TOKEN_PATH = "./token.json"
+        const code = req.query['code'];
+        console.log(code);
+
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            console.log(token);
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+                oAuth2Client.setCredentials(token);
+            });
+
+            // Store the token to disk for later program executions
+            //callback(oAuth2Client);
+        });
+        next();
+    }
 
 app.use(routes); //tells the app to handle requests using the .js files in routes
 
