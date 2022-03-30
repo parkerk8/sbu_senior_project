7e8025aa1f3d4c0ad1e573f3d443c69287ed7ff0
Jacob Knudsen <JakeKnud@protonmail.com>
2022-03-29 21:51:48 -0500

    Added to updateContact.js. Am stuck on an etag error.

--- IntegrationServer/src/featureControl/updateContact.js
+++ IntegrationServer/src/featureControl/updateContact.js
@@ -1,58 +1,103 @@
+const { google } = require('googleapis');
+const oAuth2Client = require('../temp').help
+google.options({ auth: oAuth2Client });
+var service = google.people({ version: 'v1', auth: oAuth2Client });
 const fs = require('fs');
 async function updateContactInfo(req, res) {
-    //puts monday.com data into one place
-    updateContact = {
-        ItemID: req.body.payload.inboundFieldValues.itemId,
-        ColumnID: req.body.payload.inboundFieldValues.columnId,
-        NewValue: req.body.payload.inboundFieldValues.columnValue,
-        NewVersionOfItem: req.body.payload.inboundFieldValues.itemMapping
-    }
+	//puts monday.com data into one place
+	updateContact = {
+		ItemID: req.body.payload.inboundFieldValues.itemId,
+		ColumnID: req.body.payload.inboundFieldValues.columnId,
+		//NewValue: req.body.payload.inboundFieldValues.columnValue,
+		NewValue: req.body.payload.inboundFieldValues.itemMapping.text,
+		NewVersionOfItem: req.body.payload.inboundFieldValues.itemMapping
+	}
 	console.log(updateContact);
 
-    //takes monday.com data and formats it for a json object
+	//takes monday.com data and formats it for a json object
 	json = JSON.stringify(updateContact);
 
-    //Creates/replaces the json file of data to be pushed
+	//Creates/replaces the json file of data to be pushed
 	fs.writeFile('./updateContact.json', json, (err) => {
-        if (!err) {
-            console.log('yes');
-        }
-    })
+		if (!err) {
+			console.log('yes');
+		}
+	})
 
-	
+	var itemIDsArr = fs.readFileSync("./itemIDs.txt")
+		.toString('UTF8')
+		.split('\n');
+	console.log(itemIDsArr);
+
+	var contactIDsArr = fs.readFileSync("./contactIDs.txt")
+		.toString('UTF8')
+		.split('\n');
+
+	var etagsArr = fs.readFileSync("./etags.txt")
+		.toString('UTF8')
+		.split('\n');
+
+	var contactID;
+	var contactEtag;
+	var len = itemIDsArr.length;
+
+	for (var i = 0; i < len; i++) {
+		if (updateContact.ItemID == itemIDsArr[i]) {
+			contactID = contactIDsArr[i];
+			contactEtag = etagsArr[i];
+		}
+	}
+
+	//console.log(etag);
+
+
+
 	fs.readFile('updateContact.json',
 		function (err, data) {
 			var jsonData = data;
 			var jsonParsed = JSON.parse(jsonData);
 
-			console.log(oAuth2Client);
-			service.people.createContact({
+			//console.log(oAuth2Client);
+			console.log(contactEtag);
+			service.etag = contactEtag;
+			//console.log(service.people.etag);
+			service.people.updateContact({
+				resourceName: contactID,
+				//etag: "string",
 				requestBody: {
-					names: [
-						{
-							displayName: jsonParsed.ContactName,
-							familyName: jsonParsed.ContactName,
-							//givenName: 'Tim',
-						},
-					],
+
+				names: [
+					{
+						displayName: jsonParsed.text,
+						familyName: jsonParsed.text,
+						//givenName: 'Tim',
+					},
+				],
+				emailAddresses: [
+					{
+						value: jsonParsed.NewValue.value,
+					}
+				],
+				},
+			},
+
+				(err, res) => {
+					if (err) return console.error('The API returned an error: ' + err)
+					console.log(" ");
+					console.log(res);
 				}
-			}, (err, res) => {
-				if (err) return console.error('The API returned an error: ' + err)
-				console.log(" ");
-				console.log(res);
-			}
 			);
 		}
 	)
 
-    //jsonfile.writeFile('Item ID: ', JSON.stringify(req.body.payload.inboundFieldValues.itemId)
-  
-  console.log('Item ID: ', JSON.stringify(req.body.payload.inboundFieldValues.itemId));	
-  console.log('Column ID: ', JSON.stringify(req.body.payload.inboundFieldValues.columnId));
-  console.log('New Value: ', JSON.stringify(req.body.payload.inboundFieldValues.columnValue));
-  console.log('New version of item: ', JSON.stringify(req.body.payload.inboundFieldValues.itemMapping));
-  console.log(" ");
-  return res.status(200).send({});
+	//jsonfile.writeFile('Item ID: ', JSON.stringify(req.body.payload.inboundFieldValues.itemId)
+
+	console.log('Item ID: ', JSON.stringify(req.body.payload.inboundFieldValues.itemId));
+	console.log('Column ID: ', JSON.stringify(req.body.payload.inboundFieldValues.columnId));
+	console.log('New Value: ', JSON.stringify(req.body.payload.inboundFieldValues.columnValue));
+	console.log('New version of item: ', JSON.stringify(req.body.payload.inboundFieldValues.itemMapping));
+	console.log(" ");
+	return res.status(200).send({});
 };
 
 module.exports = {
