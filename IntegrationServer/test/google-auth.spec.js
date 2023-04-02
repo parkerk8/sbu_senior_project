837c7e8a0a8910d8d453f8131ddd64bf6cde3e14
Replit user <>
2023-04-02 13:13:02 +0000

    Removed migrations test file. No need for it after research. All db files have unit tests and Tumekie's work in OAuth has been copied over. LVSP-55

--- IntegrationServer/test/google-auth.spec.js
+++ IntegrationServer/test/google-auth.spec.js
@@ -1,54 +1,69 @@
-var GoogleAuth = require("../src/OAuth/google-auth.js");
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const { expect } = chai;
-const { describe, it } = require('mocha');
+const assert = require('chai').assert;
+const OAuth2Client = require('google-auth-library').OAuth2Client;
+const googleAuth = require("../src/OAuth/google-auth.js");
 
-chai.use(chaiHttp);
+describe('OAuth2Client', function() {
+  describe('#generateAuthUrl()', function() {
+    it('should redirect to the generated URL with valid parameters', function() {
+      const oauth2Client = new OAuth2Client(
+        process.env.GOOGLE_CLIENT_ID,
+	      process.env.GOOGLE_CLIENT_SECRET,
+	      process.env.BACK_TO_URL
+      );
 
-describe('OAuth2Client generateAuthUrl', () => {
-  it('should redirect to the authorization URL with the expected parameters', async () => {
-    const expectedUrl = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive';
-    const res = {
-      redirect: (url) => {
-        expect(url).to.equal(expectedUrl);
-        done();
-      },
-      status: () => ({
-        send: () => {}
-      })
-    };
-    const OAuth2Client = new GoogleAuth.OAuth2Client({
-      clientId: '949681890895-kaoot1p07us43e1pqlrcu7li4t6vhous.apps.googleusercontent.com',
-      clientSecret: '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',
-      redirectUri: 'https://sbuseniorprojectv2.tumekie1999.repl.co/tokenHandle',
+      const res = {
+        redirect: function(url) {
+          assert.match(url, /^https:\/\/accounts\.google\.com\/o\/oauth2\/v2\/auth\?access_type=offline&scope=.+$/);
+          return this;
+        },
+        status: function(code) {
+          assert.fail(`Response status code ${code} should not have been called`);
+          return this;
+        },
+        send: function() {
+          assert.fail('Response send should not have been called');
+          return this;
+        }
+      };
+
+      try {
+        const url = oauth2Client.generateAuthUrl({
+          access_type: 'offline',
+          scope: googleAuth.SCOPES
+        });
+        res.redirect(url);
+      } catch (err) {
+        assert.fail(`An error occurred while generating the authentication URL: ${err}`);
+      }
     });
-    const generateAuthUrlStub = sinon.stub(OAuth2Client, 'generateAuthUrl').returns(expectedUrl);
 
-    await myController.generateAuthorizationUrl(req, res);
+    it('should return a 500 error if the URL cannot be generated', function() {
+      const oauth2Client = new OAuth2Client(
+        process.env.GOOGLE_CLIENT_ID,
+	      process.env.GOOGLE_CLIENT_SECRET,
+	      process.env.BACK_TO_URL
+      );
+      const res = {
+        redirect: function(url) {
+          assert.fail(`Response redirect should not have been called with URL ${url}`);
+          return this;
+        },
+        status: function(code) {
+          assert.equal(code, 500);
+          return this;
+        },
+        send: function() {
+          assert.ok(true);
+          return this;
+        }
+      };
 
-    expect(generateAuthUrlStub).to.have.been.calledWith({ access_type: 'offline', scope: SCOPES });
-  });
-
-  it('should handle errors when generating the authorization URL', async () => {
-    const errorMessage = 'Some error message';
-    const res = {
-      status: () => ({
-        send: () => {}
-      })
-    };
-    const OAuth2Client = new GoogleAuth.OAuth2Client({
-      clientId: '949681890895-kaoot1p07us43e1pqlrcu7li4t6vhous.apps.googleusercontent.com',
-      clientSecret: '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',
-      redirectUri: 'https://sbuseniorprojectv2.tumekie1999.repl.co/tokenHandle',
+      try {
+        oauth2Client.generateAuthUrl();
+      } catch (err) {
+        assert.equal(err.message, 'Invalid parameters');
+        res.status(500).send();
+      }
     });
-    const generateAuthUrlStub = sinon.stub(OAuth2Client, 'generateAuthUrl').throws(new Error(errorMessage));
-    const consoleErrorStub = sinon.stub(console, 'error');
-
-    await myController.generateAuthorizationUrl(req, res);
-
-    expect(generateAuthUrlStub).to.have.been.calledWith({ access_type: 'offline', scope: SCOPES });
-    expect(consoleErrorStub).to.have.been.calledWith('The URL could not be generated', new Error(errorMessage));
-    expect(res.status()).to.have.been.calledWith(500);
   });
 });
